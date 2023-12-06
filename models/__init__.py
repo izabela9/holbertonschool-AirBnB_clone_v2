8cad695c36ab901152c3b8c8b5e262a8615281cb
izabela9 <isabelanelaj1@gmail.com>
2023-12-06 18:04:02 +0100

    db storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
 
 
-storage = FileStorage()
-storage.reload()
+from os import getenv
+from models.amenity import Amenity
+from models.state import State
+from models.base_model import BaseModel
+from models.city import City
+from models.review import Review
+from models.user import User
+from models.place import Place
+
+if getenv("HBNB_TYPE_STORAGE") == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,30 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+
+    id = Column(String(60),
+                nullable=False,
+                unique=True,
+                primary_key=True)
+
+    created_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
+
+    updated_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
+    
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
@@ -28,6 +48,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -38,4 +59,11 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
+
+
+    def delete(self):
+        from models import storage
+        storage.delete(self)
