6afc27e28fefbfe83f8cb562bd54e93fa4ff0c2d
izabela9 <isabelanelaj1@gmail.com>
2023-12-07 18:02:24 +0100

    update files

--- models/amenity.py
+++ models/amenity.py
@@ -7,11 +7,12 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-     """Amenity class"""
+    """Amenity class"""
 
-__tablename__ = 'amenities'
-name = Column(String(128),
-                  nullable=False)
-place_amenities = relationship("Place",
+    __tablename__ = 'amenities'
+    
+    name = Column(String(128), nullable=False)
+    
+    place_amenities = relationship("Place",
                                    secondary='place_amenity',
                                    viewonly=False)
