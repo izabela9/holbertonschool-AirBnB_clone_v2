5d6e91bb494c2138bf6bf163ca6e35749fa06620
izabela9 <isabelanelaj1@gmail.com>
2023-12-12 15:02:31 +0100

    number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -51,12 +51,13 @@ def number(n):
     return f"{n} is a number"
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_temp(n):
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
     """
     Dynamic route
     """
-    return render_template('5-number.html', number=3)
+    return render_template('5-number.html', number=n)
+
 
 if __name__ == '__main__':
     app.run(debug=False)
