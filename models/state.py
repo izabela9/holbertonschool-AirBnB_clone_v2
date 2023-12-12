615c6443d7010ad094fac98bb5998bfa59d087ab
izabela9 <isabelanelaj1@gmail.com>
2023-12-12 16:43:17 +0100

    improve engines

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from models.state import Base
 from sqlalchemy import Column, Integer, \
     String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -14,13 +15,14 @@ class State(BaseModel, Base):
 
     name = Column(String(128),
                   nullable=False)
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        @property
+        def cities(self):
+            from models.city import City
+            from models import storage
 
-    cities = relationship("City", backref="state", cascade="delete")
-
-    @property
-    def cities(self):
-        from models.city import City
-        from models import storage
-
-        return [city for city in storage.all(City).values()
-                if city["state_id"] == self.id]
+            return [city for city in storage.all(City).values()
+                    if city["state_id"] == self.id]
