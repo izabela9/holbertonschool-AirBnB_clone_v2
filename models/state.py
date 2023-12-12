76ca10e025c357f01705ae3ae18710b8349cb122
izabela9 <isabelanelaj1@gmail.com>
2023-12-12 16:47:22 +0100

    improve engines

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
 
     name = Column(String(128),
                   nullable=False)
-    
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", backref="state", cascade="delete")
     else:
